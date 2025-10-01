f0eb1936b984ac26c53f54abbea1578a2444d2f1
bl <bouchkhaf@gmail.com>
2025-10-01 11:40:54 +0200

    1TestPass

---
+++ src/main/java/r5a08_findmyword/Letter.java
@@ -0,0 +1,9 @@
+package r5a08_findmyword;
+
+    public enum Letter {
+        CORRECT,  // lettre correcte et à la bonne position
+        PART_CORRECT,  // lettre correcte, mais mal placée
+        INCORRECT  // lettre incorrecte
+    }
+
+
